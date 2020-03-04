dcccc6c02fe4a2578dbfa90fab342bb6a9b8cac2
Dakotah Huey <hueydakotah@gmail.com>
2020-03-04 02:37:58 -0500

    feat: base commands to start
    * corona temp command
    
    * help command that lists all available commands
    
    * kill command to shoot users
    
    * server command to list server name and amount of users in server

---
+++ src/commands/corona.js
@@ -0,0 +1,11 @@
+function coronaVirus() {
+  return;
+}
+
+module.exports = {
+  name: "corona",
+  description: "Get lates stats on the Corona Virus",
+  execute(message, args) {
+    message.channel.send("Stats here soon™...");
+  }
+};
---
+++ src/commands/server.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "server",
+  description: "Lists the server name and amount of members",
+  execute(message, args) {
+    message.channel.send(
+      `**Server name:** ${message.guild.name}\n**Total members:** ${message.guild.memberCount}`
+    );
+  }
+};
