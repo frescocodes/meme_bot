8970ff3b103ed06dfb30272eb4cbd3820b4d0e65
unknown <iggylow1@gmail.com>
2020-03-05 01:17:05 -0500

    feat: coronavirus stat checker command
    * scrapes web for coronavirus stats using puppeteer

--- src/commands/corona.js
+++ src/commands/corona.js
@@ -1,11 +1,53 @@
-function coronaVirus() {
-  return;
-}
+const puppeteer = require('puppeteer');
+
+const siteURL = 'https://www.worldometers.info/coronavirus/';
+
+const scrape = async url => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+
+  // select element by XPath, returns array
+  // destructure first item of array into 'el'
+  // INFECTED
+  const [el] = await page.$x(
+    '/html/body/div[3]/div[2]/div[1]/div/div[4]/div/span'
+  );
+  const infectedCount = await el.getProperty('textContent');
+  const infectedRaw = await infectedCount.jsonValue();
+
+  // DEATHS
+  const [el2] = await page.$x(
+    '/html/body/div[3]/div[2]/div[1]/div/div[6]/div/span'
+  );
+  const deaths = await el2.getProperty('textContent');
+  const deathsRaw = await deaths.jsonValue();
+
+  // RECOVERED
+  const [el3] = await page.$x(
+    '/html/body/div[3]/div[2]/div[1]/div/div[6]/div/span'
+  );
+  const recovered = await el3.getProperty('textContent');
+  const recoveredRaw = await recovered.jsonValue();
+
+  browser.close();
+
+  return { infectedRaw, deathsRaw, recoveredRaw };
+};
+
+scrape(siteURL);
 
 module.exports = {
-  name: "corona",
-  description: "Get lates stats on the Corona Virus",
-  execute(message, args) {
-    message.channel.send("Stats here soon™...");
-  }
+  name: 'corona',
+  description: 'Get lates stats on the Corona Virus',
+  async execute(message, args) {
+    message.channel.send('Fetching latest Coronavirus results...').then(msg => {
+      setTimeout(() => {
+        msg.edit(
+          `Here are the latest Coronavirus statistics:\n**Infected:** ${results.infectedRaw}\n**Deaths:** ${results.deathsRaw}\n**Recovered:** ${results.recoveredRaw}`
+        );
+      }, 20000);
+    });
+    const results = await scrape(siteURL);
+  },
 };
